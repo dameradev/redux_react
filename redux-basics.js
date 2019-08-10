65f189ad10bab7d867a48a8bd7e67e8c19b044bb
=DamjanRadev <radevdamjan@gmail.com>
2019-08-10 18:28:45 +0300

    Redux basics

---
+++ redux-basics.js
@@ -0,0 +1,21 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+}
+
+// REDUCER
+const rootReducer = (state = initialState, action) => {
+  return state;
+}
+
+// STORE
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// DISPATCHING ACTION
+
+// SUBSCRIBTION
