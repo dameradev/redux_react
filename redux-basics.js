3c8d7283ce0f87a13097d2fc89a549084b953cfa
=DamjanRadev <radevdamjan@gmail.com>
2019-08-10 21:03:47 +0300

    Actions and Subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -8,6 +8,18 @@ const initialState = {
 
 // REDUCER
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state, 
+      counter: state.counter + 1
+    }
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state, 
+      counter: state.counter + action.value
+    }
+  }
   return state;
 }
 
@@ -16,6 +28,15 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// SUBSCRIPTION
+
+store.subscribe(()=>{
+  console.log('[Subscription]', store.getState());
+});
+
 // DISPATCHING ACTION
 
-// SUBSCRIBTION
+store.dispatch({type: "INC_COUNTER"});
+store.dispatch({type: "ADD_COUNTER", value: 10});
+// console.log(store.getState());
+
